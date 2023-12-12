b3bdc7218df6ffeaf11170a2f857eaf77694b578
QubeUK <lee@openforge.net>
2023-12-12 14:25:47 +0000

    added random words

--- Safe/safe.py
+++ Safe/safe.py
@@ -1,4 +1,10 @@
 import sys
+import requests
+'''
+To Do
+------
+add command line args safe -word -random
+'''
 LETTERS = {
             "A":0, "B":1, "C":2, "D":3, "E":4, "F":5, 
             "G":6, "H":7, "I":8, "J":9, "K":0, "L":1, 
@@ -7,17 +13,32 @@ LETTERS = {
             "Y":4, "Z":5        
 }
 
+
 def main():
-    codeword = get_input()
-    print(f"{codeword}")
+    gen_combo(word = get_input())
     
+    print(f"Word: {word}, Combo: COMBO")
+
 
 def get_input():
-    codeword = input("Enter 6 letter word to generate comboination: ").strip().upper()
-    if codeword.isalpha() and len(codeword) == 6:
-        return codeword
+    word = input("Enter 6 letter word to generate comboination: ").strip().upper()
+    if word.isalpha() and len(word) == 6:
+        return word
     else:
-        sys.exit(f"{codeword} is not a valid 6 letter word, please try again")
+        print(f"{word} is not a valid input, generating random combination:")
+        return random_word()
+
+
+def random_word():
+    response = requests.get("https://random-word-api.herokuapp.com/word?length=6")
+    word = "".join(response.json()).upper()
+    return word
+
+
+def gen_combo(word:str) ->str:
+    
+    combo = ""
+    return combo
 
 
 if __name__ =="__main__":
