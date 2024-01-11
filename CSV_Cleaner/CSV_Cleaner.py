96a9e345a83c2cc07928b3908e4f28d5214fa552
QubeUK <lee@openforge.net>
2024-01-11 11:29:50 +0000

    linting

--- CSV_Cleaner/CSV_Cleaner.py
+++ CSV_Cleaner/CSV_Cleaner.py
@@ -3,12 +3,18 @@ import pandas as pd
 
 
 def main():
+    """ 
+    A function to load in a CSV passed as parameter into a dataframe. 
+    Remove the time from datetime field and drop other unwanted columns.
+    Tidy up the 'Status' field to standardise contents.
+    Upon completion writes the file back to orgianl location with 'Cleaned' prefix. 
+    """
     data = pd.read_csv(sys.argv[1])
     data[["Created", "Time"]] = data["Created"].str.split(' ', expand=True)
     data[["Updated", "Time2"]] = data["Updated"].str.split(' ', expand=True)
     data.loc[data["Status"] == "In progress - 1st / 2nd Line"] = "In Progress"
     data.drop(["Issue Type", "Issue id", "Priority", "Time", "Time2"], inplace=True, axis=1)
-    data.to_csv("Cleaned_"+sys.argv[1], index=False)
+    data.to_csv("Cleaned_" + sys.argv[1], index=False)
 
 
 if __name__ == "__main__":
