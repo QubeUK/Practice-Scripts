4056426f93f6b607712b9bac52897655bd96100a
QubeUK <lee@openforge.net>
2024-01-11 10:10:02 +0000

    Fully working code

--- CSV_Cleaner/CSV_Cleaner.py
+++ CSV_Cleaner/CSV_Cleaner.py
@@ -1,14 +1,15 @@
 import sys
 import pandas as pd
 
-def main():
-    
-    data = pd.read_csv(sys.argv[1])
-    data.drop("Issue Type", inplace=True, axis=1)
-    data.drop("Issue id",  inplace=True, axis=1)
 
-    #data["Created"] = pd.to_datetime(data["Created"]).dt.date
-    #data["Time"] = pd.to_datetime(df["date"]).dt.time
-    data.info()
+def main():
+    data = pd.read_csv(sys.argv[1])
+    data[["Created", "Time"]] = data["Created"].str.split(' ', expand=True)
+    data[["Updated", "Time2"]] = data["Updated"].str.split(' ', expand=True)
+    data.loc[data["Status"] == "In progress - 1st / 2nd Line"] = "In Progress"
+    data.drop(["Issue Type", "Issue id", "Priority", "Time", "Time2"], inplace=True, axis=1)
+    data.to_csv("Cleaned_"+sys.argv[1], index=False)
+
+
 if __name__ == "__main__":
     main()
